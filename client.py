a352a7eee4d0681e9b2e11d9d3199cb4b224ec6b
amitkaushal360 <amitkaushal360@gmail.com>
2024-05-21 13:18:33 +0530

    second commit

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ import requests
 
 response=requests.post(
     "http://localhost:8000/poem/invoke",
-    json={'input':{'topic':"provide receipe of cutlet"}})
+    json={'input':{'topic':"which chutney is perfect"}})
 
 print(response.json()['output']['content'])
